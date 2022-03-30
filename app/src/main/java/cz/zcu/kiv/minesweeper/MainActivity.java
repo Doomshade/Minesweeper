38a58bcf76aac9e626cde40fdd3398202c9c21a9
Jakub Šmrha <kuba.smrha@gmail.com>
2022-03-30 22:58:14 +0200

    uhhh

--- app/src/main/java/cz/zcu/kiv/minesweeper/MainActivity.java
+++ app/src/main/java/cz/zcu/kiv/minesweeper/MainActivity.java
@@ -1,4 +1,4 @@
-package cz.kiv.minesweeper;
+package cz.zcu.kiv.minesweeper;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -6,7 +6,8 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import cz.kiv.minesweeper.game.Game;
+import cz.zcu.kiv.minesweeper.game.Game;
+import cz.zcu.kiv.minesweeper.game.impl.GameImpl;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,15 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences.Editor edit = getSharedPreferences(Game.GAME_VALUES, 0).edit();
-        edit.putInt(Game.S_DIFFICULTY, Game.Difficulty.HARD.ordinal());
-        edit.putInt(Game.S_WIDTH_AND_HEIGHT, 8);
+        SharedPreferences.Editor edit = getSharedPreferences(GameImpl.GAME_VALUES, 0).edit();
+        edit.putInt(GameImpl.S_DIFFICULTY, Game.Difficulty.HARD.ordinal());
+        edit.putInt(GameImpl.S_WIDTH, 8);
         edit.apply();
 
-        Game g = Game.createGame(this);
+        Game g = GameImpl.instantiateGame(this);
         g.generateMines();
 
         Log.d("game", "Game: " + g);
-        Log.d("game", "Mines:\n" + g.printBoard().toString());
+        Log.d("game", "Mines:\n" + g.printBoard());
     }
 }
---
+++ app/src/main/java/cz/zcu/kiv/minesweeper/game/Game.java
@@ -0,0 +1,74 @@
+package cz.zcu.kiv.minesweeper.game;
+
+/**
+ * The minesweeper game (engine)
+ *
+ * @author Jakub Šmrha
+ * @version 30. 3. 2022
+ * @since 30. 3. 2022
+ */
+public interface Game {
+    /**
+     * Sets/unsets a mine on a coord
+     *
+     * @param x the x coord
+     * @param y the y coord
+     * @throws IllegalArgumentException if the x or y is out of bounds
+     */
+    void setMine(int x, int y) throws IllegalArgumentException;
+
+    /**
+     * Reveals a field
+     *
+     * @param x the x coord
+     * @param y the y coord
+     * @return {@code true} if there was <b>NO</b> mine (the reveal was successful), {@code false} if there was a mine (the reveal was unsuccessful)
+     */
+    boolean reveal(int x, int y);
+
+    /**
+     * Toggles a flag on a field
+     *
+     * @param x the x coord
+     * @param y the y coord
+     * @return {@code true} if the flag was toggled, {@code false otherwise}
+     */
+    boolean toggleFlag(int x, int y);
+
+    /**
+     * Generates mines based on the difficulty
+     */
+    void generateMines();
+
+    /**
+     * @param x the x coord
+     * @param y the y coord
+     * @return {@code true} if x and y are in bounds, and a mine is present
+     */
+    boolean isMine(int x, int y);
+
+    /**
+     * @param x the x coord
+     * @param y the y coord
+     * @return the amount of mines on the given coord
+     */
+    int getNeighbourMines(int x, int y);
+
+    /**
+     * @return the string representation of the board
+     */
+    String printBoard();
+
+    /**
+     * The game difficulty
+     */
+    enum Difficulty {
+        EASY,
+        MEDIUM,
+        HARD;
+
+        public double getMineProbability() {
+            return (double) (ordinal() + 1) / 5;
+        }
+    }
+}
